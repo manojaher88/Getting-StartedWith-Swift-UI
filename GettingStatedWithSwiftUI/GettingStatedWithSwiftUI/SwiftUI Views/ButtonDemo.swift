3c516163351b1f8f2bfd062f2e51f3338a49e0f6
Manoj Aher <manojaher88@gmail.com>
2020-04-11 19:10:46 +0530

    Button added to code with selection and image

--- GettingStatedWithSwiftUI/GettingStatedWithSwiftUI/SwiftUI Views/ButtonDemo.swift
+++ GettingStatedWithSwiftUI/GettingStatedWithSwiftUI/SwiftUI Views/ButtonDemo.swift
@@ -10,7 +10,16 @@ import SwiftUI
 
 struct ButtonDemo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            print("Button Tapped")
+        }) {
+            Text("This is a new button")
+            Image("jupiter")
+            .resizable()
+            .scaledToFit()
+        }
+            // Can use any PrimitiveButtonStyle
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
